041bee201eb7ee2fb9de51f24e75275413e22fbf
Kenny Lee <102932377+HairyToenail@users.noreply.github.com>
2023-08-09 21:03:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,106 +1,56 @@
 import sys
+from asyncio import Queue
+from multiprocessing import RLock
+import random
 import torch
 import numpy as np
-
+import queue
 np.set_printoptions(threshold=sys.maxsize)
 # np.set_printoptions(suppress=True)
 import pandas as pd
-import pywavefront
-import pyglet
 import matplotlib.pyplot as plt
 import pyvista as pv
-from pyvista import examples
-import math
 
-def fire(a, spread_probability):
-    a[48, 30] = -1
 
+class adjMat(): #not sure if this actually works or if we need it
+    def __intit__(self, vertex, matrix):
+        self.vertex=vertex
+        self.matrix=matrix
+    def addEdge(self, source, destination):
+        self.matrix[source, destination] = 1
+        self.matrix[destination, source] =1
+
+class SynchronousQueue(object):
+
+    def __init__(self):
+        self.q = Queue(1)
+        self.put_lock = RLock()
+
+    def get(self):
+        value = self.q.get(block=True)
+        self.q.task_done()
+        return value
+
+    def put(self, item):
+        with self.put_lock:
+            self.q.put(item, block=True)
+            self.q.join()
+
+def fire(a, spread_probability, startY, startX):
+    a[startY, startX] = -1  # starting point
     # Loop through each cell in the a.
-    count = 0
     for row in range(len(a[:, 0])):
         for column in range(len(a[0, :])):
             # If the cell is not on fire, check if it is adjacent to a burning cell.
             if a[row, column] > 0:
                 for neighbor_row in range(-1, 2):
                     for neighbor_column in range(-1, 2):
-                        count += 1
-                    if 0 <= row + neighbor_row < len(a[:, 0]) and 0 <= column + neighbor_column < len(a[0, :]):
-                        if a[row + neighbor_row][column + neighbor_column] == 1:
+                        if 0 <= row + neighbor_row < len(a[:, 0]) and 0 <= column + neighbor_column < len(a[0, :]):
+                            if a[row + neighbor_row, column + neighbor_column] <  0 and a[row, column]>0:
                             # If it is adjacent to a burning cell, set it on fire with probability `spread_probability`.
-                            if np.random.random() < spread_probability:
-                                a[row, column] = -1
-def distance(point1, point2):
-    return np.sqrt(np.sum((point1 - point2) ** 2))
-
-
-def ant_colony_optimization(points, n_ants, n_iterations, alpha, beta, evaporation_rate, Q):
-    n_points = len(points)
-    pheromone = np.ones((n_points, n_points))
-    best_path = None
-    best_path_length = np.inf
-
-    for iteration in range(n_iterations):
-        paths = []
-        path_lengths = []
-
-        for ant in range(n_ants):
-            visited = [False] * n_points
-            current_point = np.random.randint(n_points)
-            visited[current_point] = True
-            path = [current_point]
-            path_length = 0
-
-            while False in visited:
-                unvisited = np.where(np.logical_not(visited))[0]
-                probabilities = np.zeros(len(unvisited))
-
-                for i, unvisited_point in enumerate(unvisited):
-                    probabilities[i] = pheromone[current_point, unvisited_point] ** alpha / distance(
-                        points[current_point], points[unvisited_point]) ** beta
-
-                probabilities /= np.sum(probabilities)
-
-                next_point = np.random.choice(unvisited, p=probabilities)
-                path.append(next_point)
-                path_length += distance(points[current_point], points[next_point])
-                visited[next_point] = True
-                current_point = next_point
-
-            paths.append(path)
-            path_lengths.append(path_length)
-
-            if path_length < best_path_length:
-                best_path = path
-                best_path_length = path_length
-
-        pheromone *= evaporation_rate
-
-        for path, path_length in zip(paths, path_lengths):
-            for i in range(n_points - 1):
-                pheromone[path[i], path[i + 1]] += Q / path_length
-            pheromone[path[-1], path[0]] += Q / path_length
-
-    fig = plt.figure(figsize=(8, 6))
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(points[:, 0], points[:, 1], points[:, 2], c='r', marker='o')
-
-    for i in range(n_points - 1):
-        ax.plot([points[best_path[i], 0], points[best_path[i + 1], 0]],
-                [points[best_path[i], 1], points[best_path[i + 1], 1]],
-                [points[best_path[i], 2], points[best_path[i + 1], 2]],
-                c='g', linestyle='-', linewidth=2, marker='o')
-
-    ax.plot([points[best_path[0], 0], points[best_path[-1], 0]],
-            [points[best_path[0], 1], points[best_path[-1], 1]],
-            [points[best_path[0], 2], points[best_path[-1], 2]],
-            c='g', linestyle='-', linewidth=2, marker='o')
-
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
-    plt.show()
-
-
+                                if np.random.random() < spread_probability:
+                                    a[row, column] = a[row + neighbor_row, column + neighbor_column]-1
+    return a
 
 mesh = pv.read("scan_2.obj")
 # mesh = mesh.rotate_x(180, transform_all_input_vectors=True, inplace=True)
@@ -189,11 +139,11 @@ pl.enable_surface_point_picking(callback=callback, show_point=False)
 pl.show()
 # -------------------------------------------------------------------
 #Fire simulation
-count = 0
-while not((a!=-1).all()):
-    fire(a, 0.5)
-    if(count%5000==0):
-        np.savetxt(f"fire{count}.csv", a, delimiter=',')
-    count+=1
-
-# ant_colony_optimization(a, 500, 50032, 21, 11, 0.5, 3)
+fg = a
+cY = random.randint(0, len(a[:, 0]))
+cX = random.randint(0, len(a[0, :]))
+for i in range(100):
+    fire(fg, 0.1, cY, cX)
+    if(i%10==0):
+        np.savetxt(f"fire{i}.csv", fg, delimiter=',')
+print(True)
